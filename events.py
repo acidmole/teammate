30135eda8075063eb13c3f5d35a845dd36b1bdb5
Heikki Venhola <heikki.venhola@helsinki.fi>
2021-02-07 23:12:13 +0200

    data add to Heroku database

--- events.py
+++ events.py
@@ -58,3 +58,16 @@ def add_event(type, day, time, name, location):
     db.session.execute(sql, {"type":type, "day":day, "time":time, "name":name, "location":location})
     db.session.commit()
     return True
+
+
+def adder():
+    db.session.execute("INSERT INTO game_stats (event_id, player_id, mins, fg, fg_a, three, three_a, ft, ft_a, dreb, oreb, ass, tover, steal, block) "\
+		       "VALUES ('2', '1', '23:32', '4', '5', '0', '3', '2', '2', '7', '4', '1', '3', '0', '4'")
+    db.session.execute("INSERT INTO game_stats (event_id, player_id, mins, fg, fg_a, three, three_a, ft, ft_a, dreb, oreb, ass, tover, steal, block) "\
+		       "VALUES ('2', '2', '32:15', '8', '19', '2', '7', '6', '11', '3', '2', '6', '5', '2', '0'")
+    db.session.execute("INSERT INTO game_stats (event_id, player_id, mins, fg, fg_a, three, three_a, ft, ft_a, dreb, oreb, ass, tover, steal, block) "\
+		       "VALUES ('2', '3', '18:55', '3', '4', '0' '0', '1', '1', '3', '6', '2', '7', '1', '1'")
+    db.session.execute("INSERT INTO game_stats (event_id, player_id, mins, fg, fg_a, three, three_a, ft, ft_a, dreb, oreb, ass, tover, steal, block) "\
+		       "VALUES ('2', '4', '24:02', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '0', '3'")
+    
+
--- routes.py
+++ routes.py
@@ -120,3 +120,9 @@ def single_game_stats(id):
     summed = stats.get_single_game_summary_stats(id)
     return render_template("game_stats.html", stats=player_stats, summed=summed)
 
+@app.route("/add_some_stats")
+def stat_adder():
+    if events.adder():
+        return redirect("/stats")
+    else:
+        return render_template("error.html", message="error")
